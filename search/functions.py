4c6a7719a30ff91cb7c6a65c0ac182a4c04c2ca7
g-lamanna <gabe.lamanna@outlook.com>
2020-07-07 15:58:27 -0400

    html and css page for home search engine page

---
+++ search/functions.py
@@ -0,0 +1,7 @@
+from squares import exp
+
+lst = [1,2,3,4,5,6]
+
+for i in lst:
+    if i % 2 == 0:
+        print(exp(i))
---
+++ search/squares.py
@@ -0,0 +1,12 @@
+def exp(i):
+    square = i*i
+    return square
+
+def hello():
+    print("hello")
+
+if __name__ == "__main__":
+    exp()
+    hello()
+        
+
